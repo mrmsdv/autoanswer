2588dbe884cff7f6f51fa6c1850d4af08732641d
Mr_MSDV <37757663+mrmsdv@users.noreply.github.com>
2019-10-22 06:50:37 +0800

    Update persegipanjang.py

--- persegipanjang.py
+++ persegipanjang.py
@@ -7,14 +7,14 @@ c="\033[36m"
 y="\033[33m"
 P = print
 Os = os.system
-def psgp():
+    def psgp():
         P(f' {g}Bangun datar ( Persegi Panjang )')
         P(f' {g}Yang di Ketahui pada soal ?')
         P(f' {r}Note : {y}Jika tidak ada di soal tekan Enter saja')
-        panjang = input(' panjang  = ')
-        lebar   = input(' lebar    = ')
-        luas    = input(' Luas     = ')
-        Keliling= input(' Keliling = ')
+        panjang = input(' panjang = ')
+        lebar = input(' lebar = ')
+        luas = input(' Luas = ')
+        Keliling = input(' Keliling = ')
         if luas == '' and Keliling == '':#jika panjang dan lebar di ketahui
             panjang = int(panjang)
             lebar = int(lebar)
@@ -38,11 +38,11 @@ def psgp():
             lebar = (luas/panjang)
             P(f'{y} keliling = {c}',2*(panjang+lebar))
             P(f'{y} lebar    = {c}',lebar)
-        elif keliling == '' and panjang =='':#jika luas dan lebar di ketahui
+        elif Keliling == '' and panjang =='':#jika luas dan lebar di ketahui
             luas = int(luas)
             lebar = int(lebar)
             panjang = luas/lebar
-            P(f'{y} Luas  = {c}',panjang*lebar)
-            P(f'{y} lebar = {c}',panjang)
+            P(f'{y} keliling  = {c}',2*(panjang+lebar))
+            P(f'{y} panjang   = {c}',panjang)
         else:pass
 psgp()        
